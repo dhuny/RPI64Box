640d8ab2fcfb215c259cabc4085f3410403fd0f9
dhuny <42516995+dhuny@users.noreply.github.com>
2022-11-03 06:57:24 +0400

    Add files via upload

---
+++ roles/rubbos/files/var/www/RUBBoS/PHP/config.php
@@ -0,0 +1,16 @@
+
+<?php
+
+/**
+ * Database configuration
+ *   host:     The MySQL server host
+ *   username: Username to use
+ *   password: Password of the user
+ *   db_name:  Database to use
+ */
+$DB_CONFIG = array(
+  'host' => 'localhost',
+  'username' => 'rpi64box',
+  'password' => 'Rpi64box4$',
+  'db_name' => 'rubbos'
+);
